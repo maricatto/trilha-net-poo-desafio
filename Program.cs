7947fa93e5fa3b5f23e9fcab5429cd36ee101c92
Vinícius Maricatto <vmaricatto@gmail.com>
2023-05-31 20:31:30 -0300

    Update Program.cs
    Atualizando o arquivo original com as requisições proposta pelo exercício.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
+class Program
+{
+   public static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero:"1234567890", modelo: "Nokia 3310", imei: "1234", memoria:16);
+        nokia.InstalarAplicativo("WhatsApp");
+        nokia.Ligar();
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone iPhone:");
+        Smartphone iphone = new iPhone(numero:"0987654321", modelo:"iPhone 12", imei:"5678", memoria:64);
+        iphone.InstalarAplicativo("Instagram");
+        iphone.ReceberLigacao();
+    }
+}
+    
