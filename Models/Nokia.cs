b2fe59088e1f557fd390a61157dc662a74163acc
Vinícius Maricatto <vmaricatto@gmail.com>
2023-05-31 20:30:02 -0300

    Update Nokia.cs
    Atualizando o arquivo original com as requisições proposta pelo exercício.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+  public class Nokia : Smartphone
+  {
+    public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
     }
+    public override void InstalarAplicativo(string aplicativo)
+    {
+      Console.WriteLine($"Instalando o aplicativo {aplicativo} no celular Nokia");
+      // Implementação específica para instalação de aplicativos no celular Nokia
+    }
+  }
 }
